53efb245d9314e39ab464d9f720704d522605783
Michael Chong <mikec0920@gmail.com>
2023-11-22 14:49:51 -0800

    Create 3x3 board

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function GameBoard () {
+function GameBoard() {
+  const dim = 3;
+  const board = [];
+  for (let i = 0; i < dim; i++) {
+    board[i] = [];
+    for (let j = 0; j < dim; j++) {
+      board[i].push(Cell());
+    }
+  }
+};
+
+function GameController() {
 
 };
 
-function GameController () {
-
-};
-
-function Player (name, marker) {
+function Player(name, marker) {
 
 };
