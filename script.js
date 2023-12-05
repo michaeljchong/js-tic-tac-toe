e3d816c3a123251d26a8c4ff9aabd313d4ad4066
Michael Chong <mikec0920@gmail.com>
2023-12-04 16:17:37 -0800

    Revise board structure and implement private functions to access board

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function GameBoard() {
-  const dim = 3;
-  const board = [];
-  for (let i = 0; i < dim; i++) {
-    board[i] = [];
-    for (let j = 0; j < dim; j++) {
-      board[i].push(Cell());
-    }
-  }
+  // Board is an array with row 1 being indexes 0-2, row 2 being indexes 3-5, row 3 being indexes 6-8
+  const board = Array(9);
+  board.fill(0);
+
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    console.log(board);
+  };
+
+  return {getBoard, printBoard};
 };
 
 function GameController() {
